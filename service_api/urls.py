fd635c368b85b1a4b1fcd0c4c16acb1cfce87668
S.Mohammad Mousavi <smmousavisp@gmail.com>
2021-02-03 00:23:17 +0330

    Add swagger-ui api docs to urls

--- service_api/urls.py
+++ service_api/urls.py
@@ -1,11 +1,22 @@
 from django.urls import include, path
+from django.views.generic import TemplateView
+
+from rest_framework.schemas import get_schema_view
 
 from service_api import views
-
 
 
 urlpatterns = [
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 
     path('users/', views.UserList.as_view()),
+    path('openapi', get_schema_view(
+        title="Your Project",
+        description="API for all things …",
+        version="1.0.0"
+    ), name='openapi-schema'),
+    path('swagger-ui/', TemplateView.as_view(
+        template_name='swagger-ui/swagger-ui.html',
+        extra_context={'schema_url': 'openapi-schema'}
+    ), name='swagger-ui'),
 ]
