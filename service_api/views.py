8510b95685204585e8ec1258c1b1c0c246288fad
S.Mohammad Mousavi <smmousavisp@gmail.com>
2021-02-02 19:34:38 +0330

    Add view to create and show list of Users

--- service_api/views.py
+++ service_api/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.contrib.auth.models import User
 
-# Create your views here.
+from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
+
+from service_api.serializers import UserSerializer
+
+
+class UserList(generics.ListCreateAPIView):
+    permission_classes = (IsAuthenticated, IsAdminUser,)
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
