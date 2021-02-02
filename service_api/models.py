81636082620f9bf73e1c78572e76fba989add7f3
S.Mohammad Mousavi <smmousavisp@gmail.com>
2021-02-02 19:31:00 +0330

    Add Profile class to models

--- service_api/models.py
+++ service_api/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.core.validators import RegexValidator
 
-# Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    birth_date = models.DateField(blank=True, null=True)
+    GENDER_CHOICES = (
+        ('M', 'Male'),
+        ('F', 'Female'),
+    )
+    gender = models.CharField(max_length=1, choices=GENDER_CHOICES)
+    phone_regex = RegexValidator(regex=r'^\+?1?\d{9,15}$', message="format: '+999999999'. Up to 15 digits allowed.")
+    phone_number = models.CharField(validators=[phone_regex], max_length=17, blank=True)
+    city = models.CharField(max_length=255)
+    is_manager = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.user.first_name + ' ' + self.user.last_name
