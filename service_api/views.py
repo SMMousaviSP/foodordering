4eb160d165f0c3be5ec6b9512337030acce53241
S.Mohammad Mousavi <smmousavisp@gmail.com>
2021-02-03 00:42:10 +0330

    Add login and logout view

--- service_api/views.py
+++ service_api/views.py
@@ -1,9 +1,31 @@
 from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login
 
 from rest_framework import generics
-from rest_framework.permissions import IsAuthenticated, IsAdminUser
-
+from rest_framework.permissions import IsAuthenticated, IsAdminUser, AllowAny
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.decorators import api_view, permission_classes
 from service_api.serializers import UserSerializer
+
+
+@api_view(['POST'])
+@permission_classes([AllowAny])
+def api_login(request):
+    username = request.data['username']
+    password = request.data['password']
+    user = authenticate(request, username=username, password=password)
+    if user is not None:
+        login(request, user)
+        return Response(status=status.HTTP_200_OK)
+    return Response(status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['GET'])
+@permission_classes([AllowAny])
+def api_logout(request):
+    request.session.flush()
+    return Response(status=status.HTTP_200_OK)
 
 
 class UserList(generics.ListCreateAPIView):
