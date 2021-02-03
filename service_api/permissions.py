10a9463d6ad7801e4dc913da5bba721e936269a8
S.Mohammad Mousavi <smmousavisp@gmail.com>
2021-02-03 22:55:23 +0330

    Add a permission to check if manager has created restaurant

--- service_api/permissions.py
+++ service_api/permissions.py
@@ -1,5 +1,7 @@
 from rest_framework import permissions
 
+from service_api.models import Restaurant
+
 
 class ManagerPermission(permissions.BasePermission):
     """
@@ -9,3 +11,13 @@ class ManagerPermission(permissions.BasePermission):
         if request.user.is_staff:
             return True
         return request.user.profile.is_manager
+
+
+class HasRestaurant(permissions.BasePermission):
+    """
+    Check if the manager has created a restaurant to manage.
+    """
+    message = "You should create a restaurant first to be able to access it's foods"
+
+    def has_permission(self, request, view):
+        return bool(Restaurant.objects.filter(manager=request.user.pk).first())
