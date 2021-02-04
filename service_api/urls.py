77ee52b8fdae48a798c5f7b6acc279fd7b1e85aa
S.Mohammad Mousavi <smmousavisp@gmail.com>
2021-02-04 16:26:15 +0330

    Add list of customers active orders to urls

--- service_api/urls.py
+++ service_api/urls.py
@@ -34,5 +34,6 @@ urlpatterns = [
     path("newrestaurant/", views.CreateRestaurant.as_view()),
     path("managerfoods/", views.ManagerFoodListCreate.as_view()),
     path("updatefood/<int:pk>/", views.UpdateFood.as_view()),
-    path("neworder/", views.CreateOrder.as_view())
+    path("neworder/", views.CreateOrder.as_view()),
+    path("customer/activeorders/", views.CustomerActiveOrderList.as_view()),
 ]
