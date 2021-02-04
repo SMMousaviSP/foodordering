4837d40b351674a65d7770f89ad32201d6eb955e
S.Mohammad Mousavi <smmousavisp@gmail.com>
2021-02-04 16:36:49 +0330

    Add list of customers cancelled orders to urls

--- service_api/urls.py
+++ service_api/urls.py
@@ -6,11 +6,13 @@ from rest_framework.schemas import get_schema_view
 from service_api import views
 
 
-CUSTOMER_PREFIX = 'customer'
-MANAGER_PREFIX = 'manager'
+CUSTOMER_PREFIX = "customer"
+MANAGER_PREFIX = "manager"
 
 urlpatterns = [
+    # rest_framework Authentication
     path("api-auth/", include("rest_framework.urls", namespace="rest_framework")),
+    # OpenAPI and Swagger UI for API documentations
     path(
         "openapi",
         get_schema_view(
@@ -28,15 +30,22 @@ urlpatterns = [
         ),
         name="swagger-ui",
     ),
+    # General API URI
     path("register/", views.Register.as_view()),
     path("login/", views.api_login.as_view()),
     path("logout/", views.api_logout),
     path("users/", views.UserList.as_view()),
     path("profile/", views.UserProfile.as_view()),
     path("restaurants/", views.RestaurantList.as_view()),
+    # Managers API URI
     path(f"{MANAGER_PREFIX}/newrestaurant/", views.CreateRestaurant.as_view()),
     path(f"{MANAGER_PREFIX}/foods/", views.ManagerFoodListCreate.as_view()),
     path(f"{MANAGER_PREFIX}/updatefood/<int:pk>/", views.UpdateFood.as_view()),
+    # Customers API URI
     path(f"{CUSTOMER_PREFIX}/neworder/", views.CreateOrder.as_view()),
     path(f"{CUSTOMER_PREFIX}/activeorders/", views.CustomerActiveOrderList.as_view()),
+    path(
+        f"{CUSTOMER_PREFIX}/cancelledorders/",
+        views.CustomerCancelledOrderList.as_view(),
+    ),
 ]
